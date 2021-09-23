19d48bb2802a1de2494f28927a15d3193cbf123d
cicarrascog <carlos.carrasco@anu.edu.au>
2021-09-23 15:16:03 +1000

    Create Element_Data.R
    Creates documentation for Data

---
+++ R/Element_Data.R
@@ -0,0 +1,25 @@
+#' Element data for calculations
+#'
+#' A dataset containing CI and Mantle values for normalization for selected elements. This is the result of `Create_Element_Data()` function. The data used is from IUPAC, Palme and O'Neill (2014), and McDonough and Sun (1995). Ionic Radii are from Shannon (1976).
+#'
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{Z}{Atomic Number}
+#'   \item{Element_name}{Element Symbol}
+#'   \item{Atomic_Mass}{Atomic Mass from IUPAC}
+#'   \item{Unit}{Measure Unit of the Concentrations, ppm = parts per million, pct = percentage}
+#'   \item{Oneill2014CI}{Chondrite values from Palme and Oneil (2014)}
+#'   \item{Oneill2014CI_RSD}{Uncertainty from chondrite values from palme and Oneil (2014) as RSD (Relative standard Deviation)}
+#'   \item{Oneill2014Mantle}{Primitive Mantle values from Palme and Oneil (2014)}
+#'   \item{Oneill2014Mantle_RSD}{Uncertainty from Primitive Mantlevalues from palme and Oneil (2014) as RSD (Relative standard Deviation)}
+#'   \item{McDonough1995CI}{Chondrite values from Mcdonough and Sun (1995)}
+#'   \item{ShannonRadiiVIII_Coord_3plus}{Shannon (1976) Ionic Radii for elements in Eightfold coordination and 3+ charge}
+#'   ...
+#' }
+#' @source IUPAC Website (\url{https://iupac.org/})
+#' @source Palme, H., and O’Neill, H. St. C., 2014, 3.1 - Cosmochemical Estimates of Mantle Composition, in Holland, H. D. and Turekian, K. K. eds., Treatise on Geochemistry (Second Edition): Oxford, Elsevier, p. 1-39.(\url{https://doi.org/10.1016/B978-0-08-095975-7.00201-1})
+#' @source McDonough, W. F., and Sun, S. -s., 1995, The composition of the Earth: Chemical Geology, v. 120, p. 223-253.(\url{https://doi.org/10.1016/0009-2541(94)00140-4})
+#' @source Shannon, R. D., 1976, Revised effective ionic radii and systematic studies of interatomic distances in halides and chalcogenides: Acta Crystallographica Section A, v. 32, p. 751-767. \url{ https://doi.org/10.1107/S0567739476001551}
+#'
+"Element_Data"
