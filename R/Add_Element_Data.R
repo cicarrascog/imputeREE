24ae5e962adcd6800b89279645438f09c86bf796
cicarrascog <carlos.carrasco@anu.edu.au>
2021-09-29 11:27:28 +1000

    Update Add_Element_Data.R

--- R/Add_Element_Data.R
+++ R/Add_Element_Data.R
@@ -1,4 +1,4 @@
-Add_Element_data <- function(dat, prefix = "Zr", sufix = "ppm_Normalized", Element_list = REE_plus_Y_Elements, add_ID = FALSE, add_all = FALSE) {
+Add_Element_data <- function(dat, preffix = NULL, suffix = NULL, Element_list = REE_plus_Y_Elements, add_ID = FALSE, add_all = TRUE) {
 
 
   if (!is.data.frame(dat)) {
@@ -6,46 +6,23 @@ Add_Element_data <- function(dat, prefix = "Zr", sufix = "ppm_Normalized", Eleme
   }
 
   ## add ID
+
   if (add_ID == TRUE) {
 
     dat <- dat %>% Add_ID(var = var)
 
   }
 
+ dat <-  CleanColnames(suffix = suffix, preffix = preffix ) %>%
+   dplyr::select(rowid, paste0('^',Element_list, '$'))
+
   original <- dat
 
-  ## matches names
-
-  Element_list <- paste(prefix, Element_list, sufix, sep = "_")
-
-  if (is.null(prefix)) {
-    Element_list <- gsub("^_", "", Element_list)
-  }
-
-  if (is.null(sufix)) {
-    Element_list <- gsub("_$", "", Element_list)
-  }
-
-  ## matches names in the elemental data
-  Element_Data <- Element_Data %>%
-    dplyr::mutate(Element_name = paste(prefix, Element_name, sufix, sep = "_"))
 
 
-  if (is.null(prefix)) {
-    Element_Data <- Element_Data %>% dplyr::mutate(Element_name = stringr::str_remove(Element_name, "^_"))
-  }
+   ## matches names in the elemental data
 
-  if (is.null(sufix)) {
-    Element_Data <- Element_Data %>% dplyr::mutate(Element_name = stringr::str_remove(Element_name, "_$"))
-  }
-
-  if (add_all == TRUE) {
-    Element_Data <- Element_Data
+Element_Data <- Element_Data
 
     dat %>% dplyr::left_join(., Element_Data, by = "Element_name")
-  } else {
-
-  Element_Data <- Element_Data %>% dplyr::select(ShannonRadiiVIII_Coord_3plus, Element_name) }
-
-  dat %>% dplyr::left_join(., Element_Data, by = "Element_name")
 }
