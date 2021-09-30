6bce75f6e51cf2d8520d3ca623c049801c71e799
cicarrascog <carlos.carrasco@anu.edu.au>
2021-09-30 14:28:51 +1000

    Update Add_Element_Data.R
    Function was simplified to work with long format

--- R/Add_Element_Data.R
+++ R/Add_Element_Data.R
@@ -1,28 +1,11 @@
-Add_Element_data <- function(dat, preffix = NULL, suffix = NULL, Element_list = REE_plus_Y_Elements, add_ID = FALSE, add_all = TRUE) {
-
+Add_Element_data <- function(dat) {
 
   if (!is.data.frame(dat)) {
     stop("dat should be a dataframe, you provided:", class(dat)[1])
   }
 
-  ## add ID
-
-  if (add_ID == TRUE) {
-
-    dat <- dat %>% Add_ID(var = var)
-
-  }
-
- dat <-  CleanColnames(suffix = suffix, preffix = preffix ) %>%
-   dplyr::select(rowid, paste0('^',Element_list, '$'))
-
-  original <- dat
-
-
-
    ## matches names in the elemental data
 
 Element_Data <- Element_Data
-
     dat %>% dplyr::left_join(., Element_Data, by = "Element_name")
 }
